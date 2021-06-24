a9320e884b83284357a2784d1fef7391e2ad7630
Begin <support@begin.com>
2021-06-24 17:27:21 +0000

    ci: so it Begins!

---
+++ app.js
@@ -0,0 +1,81 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require('mongoose');
+
+const homeStartingContent = "Welcome, you beautiful people! This is my daily journal where I post blogs about different things. You can also be a part of my Daily Journal, just go to Compose Blog section and write about yourself, introduce yourself or share something useful so that, we all can learn something new. And let's build Journal of valuable content =) ";
+const aboutContent = "I'm Final Year student pursuing B.tech computer science from Amity University, Noida. Designs and patterns have always fascinated me and pushed me to do some creativity. So, here I am creating something and learning new things daily.";
+const contactContent = "Website - https://twishasharma.info , LinkedIn - https://www.linkedin.com/in/twisha-sharma-4509a91b2/ , Github - https://github.com/Itstwisha , Mail - twishasharma1720@gmail.com";
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/blogDB", {useNewUrlParser: true});
+
+const postSchema = {
+  title: String,
+  content: String
+};
+
+const Post = mongoose.model("Post", postSchema);
+
+app.get("/", function(req, res){
+
+  Post.find({}, function(err, posts){
+    res.render("home", {
+      startingContent: homeStartingContent,
+      posts: posts
+      });
+  });
+});
+
+app.get("/compose", function(req, res){
+  res.render("compose");
+});
+
+app.post("/compose", function(req, res){
+  const post = new Post({
+    title: req.body.postTitle,
+    content: req.body.postBody
+  });
+
+
+  post.save(function(err){
+    if (!err){
+        res.redirect("/");
+    }
+  });
+});
+
+app.get("/posts/:postId", function(req, res){
+
+const requestedPostId = req.params.postId;
+
+  Post.findOne({_id: requestedPostId}, function(err, post){
+    res.render("post", {
+      title: post.title,
+      content: post.content
+    });
+  });
+
+});
+ 
+
+app.get("/about", function(req, res){
+  res.render("about", {aboutContent: aboutContent});
+});
+
+app.get("/contact", function(req, res){
+  res.render("contact", {contactContent: contactContent});
+});
+
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
